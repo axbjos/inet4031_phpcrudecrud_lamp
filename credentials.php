473232670880a39ec988a9d895d154a524bee148
Joe Axberg <joeaxberg@axnetlabs.org>
2022-09-28 18:22:41 -0500

    set db connection to "localhost"

--- credentials.php
+++ credentials.php
@@ -1,5 +1,5 @@
 <?php                            //connection & credentials
-$servername = "192.168.56.11";      //mysql is on the same host as apache
+$servername = "localhost";      //mysql is on the same host as apache
 $dbname = "employees";          //which db you're going to use
 $username = "joeaxberg";
 $password = "abc123";
