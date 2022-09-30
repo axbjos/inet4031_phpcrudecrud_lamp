f4e91ccd9fe51b96298f46fceb3d2d4f96dbe5b8
Joe Axberg <joeaxberg@axnetlabs.org>
2022-09-30 07:47:32 -0500

    setting mysql user to a generic

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php                            //connection & credentials
 $servername = "localhost";      //mysql is on the same host as apache
 $dbname = "employees";          //which db you're going to use
-$username = "joeaxberg";
+$username = "phpuser1";
 $password = "abc123";
 ?>
