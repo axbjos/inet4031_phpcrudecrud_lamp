144bf569aa9d2b3a74e7a94554e9c9eadd39bde3
Joe Axberg <joeaxberg@axnetlabs.org>
2023-11-15 13:40:53 -0600

    Update credentials.php - set to db conn to localhost by default

--- credentials.php
+++ credentials.php
@@ -1,5 +1,5 @@
 <?php
-$servername = '192.168.110.132';     //if apache on diff server from db or using Docker, set IP address
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
 $dbname = 'employees';              //which db you're going to use
 $username = 'phpuser';
 $password = 'dbuser123';
