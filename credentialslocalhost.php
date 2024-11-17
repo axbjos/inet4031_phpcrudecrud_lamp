46f50908afa8eadf826196fdffc3e3c0a7ca32e5
Joe Axberg <jaxberg@Joes-MBP.localdomain>
2024-11-16 18:11:27 -0600

    removed the extra credentials files

--- credentialslocalhost.php
+++
@@ -1,6 +0,0 @@
-<?php                            //connection & credentials
-$servername = "localhost";      //mysql is on the same host as apache
-$dbname = "employees";          //which db you're going to use
-$username = "phpuser1";
-$password = "abc123";
-?>
